847c48d05139dc961d5efcebc0c3f8b985b84216
Israel Guevara Torres <israelguevara1492@gmail.com>
2024-11-28 11:19:18 -0500

    feat: descripcion del planeta, arreglo de cadenas

--- src/App.java
+++ src/App.java
@@ -7,4 +7,16 @@ public class App {
         "Saturno", "Urano", "Neptuno", "Plutón", "Ceres", 
         "Eris", "Haumea", "Makemake"
     };
+   
+    static String[] descripcionPlaneta = {
+        "Cocinándose a temperaturas extremas.", "Está densa la atmósfera.", 
+        "De aquí vienen los comunistas (el planeta rojo).", 
+        "Tormentas, gases y vientos de más de 500 Km/h.", 
+        "El señor de los anillos, y lunas.", "Un gigante de hielo.",
+        "Sopla, sopla, sopla\nlos vientos más veloces del sistema solar.", 
+        "Enano, de órbita única y elíptica", "El pequeñito del cinturón de asteroides.",
+        "Enano al borde del sistema.", "Rotación rápida, elíptico y enano.", 
+        "El último enano de hielo."
+    };
+
 }    
